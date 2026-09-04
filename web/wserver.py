816bf3940251bc8b0dc3f6849ad5fe17dc24073a
urcap <sneriokate.r@gmail.com>
2026-09-05 02:01:15 +0530

    fix: missing ultra.css + static mount; bsetting bool vars for add-on toggles

--- web/wserver.py
+++ web/wserver.py
@@ -103,6 +103,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+from os import path as _ospath
+
+from fastapi.staticfiles import StaticFiles
+
+if _ospath.isdir("web/static"):
+    app.mount("/static", StaticFiles(directory="web/static"), name="static")
+
 
 templates = Jinja2Templates(directory="web/templates/")
 
